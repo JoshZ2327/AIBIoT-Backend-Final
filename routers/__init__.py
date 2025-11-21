192c0fe82cc95702cb633b60da44a5ae2cdb546e
JoshZ2327 <joshua@aprilinvestments.xyz>
2025-11-20 19:36:12 -0500

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter
 
+from routers.voice import router as voice_router
+from routers.prediction import router as prediction_router
+from routers.websocket import router as websocket_router
+
+api_router = APIRouter()
+
+# Register sub-routers
+api_router.include_router(voice_router, tags=["Voice Commands"])
+api_router.include_router(prediction_router, tags=["AI Predictions"])
+api_router.include_router(websocket_router, tags=["WebSockets"])
