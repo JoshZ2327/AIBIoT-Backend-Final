4325f3c0ab9d475284c26d6a819f92cd72a2c5ec
JoshZ2327 <joshua@aprilinvestments.xyz>
2025-11-20 21:26:41 -0500

    Update anomaly_detection.py

--- utils/anomaly_detection.py
+++ utils/anomaly_detection.py
@@ -1,1 +1,7 @@
+# utils/anomaly_detection.py
 
+import random
+
+def detect_anomalies(sensor_name: str) -> bool:
+    # Placeholder: randomly simulate anomaly
+    return random.random() < 0.1  # 10% chance
